38862c50b615f0c0c8788c882311972a3461b96d
Niyazi Berkay Çokuysal <48411015+niyazicokuysal@users.noreply.github.com>
2020-07-03 23:36:41 +0300

    Commit02

---
+++ Unity_ExerciseProject02/Assets/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float spawnRate = 1f;
+
+    public GameObject hexagonObj;
+
+    private float nextTo = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= nextTo)
+        {
+            Instantiate(hexagonObj, Vector3.zero, Quaternion.identity);
+            nextTo = Time.time + 1f / spawnRate;
+        }
+    }
+}
