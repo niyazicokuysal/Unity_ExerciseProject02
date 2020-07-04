47d73c7954fa59e398ec42c325e760c5bb7ed950
Niyazi Berkay Çokuysal <48411015+niyazicokuysal@users.noreply.github.com>
2020-07-04 20:18:03 +0300

    Commit03

---
+++ Unity_ExerciseProject02/Assets/Scipts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 600f;
+
+    float movement = 0f;
+
+    public static int point = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+    }
+
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Collider") {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            point = 0;
+        }
+        else
+        {
+            point++;
+            FindObjectOfType<AudioManager>().Play("Snap");
+
+        }
+
+        
+        
+    }
+}
---
+++ Unity_ExerciseProject02/Assets/Scipts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float spawnRate = 1f;
+
+    public GameObject hexagonObj;
+    public GameObject squareObj;
+
+
+    private float nextTo = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= nextTo)
+        {
+            if(Player.point > 7)
+            {
+                Instantiate(hexagonObj, Vector3.zero, Quaternion.identity);
+                nextTo = Time.time + 1f / spawnRate;
+            }
+            else
+            {
+                Instantiate(squareObj, Vector3.zero, Quaternion.identity);
+                nextTo = Time.time + 1f / spawnRate;
+            }
+            
+        }
+    }
+}
